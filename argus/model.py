78109de7bb338bd0025db9a4ef25b25e915227af
lRomul <ruslan1123@gmail.com>
2018-07-06 13:56:32 +0300

    feat: Add model methods definition

--- argus/model.py
+++ argus/model.py
@@ -22,3 +22,18 @@ class State(object):
 class Model(BuildModel):
     def __init__(self, params):
         super().__init__(params)
+
+    def fit(self, train_loader, val_loader, max_epochs=None, handlers=None):
+        pass
+
+    def set_lr(self, lr):
+        pass
+
+    def save_model(self, file_path):
+        pass
+
+    def validate(self, val_loader):
+        pass
+
+    def predict(self, input_):
+        pass
