ecab064ace361f7b1b63866428ba2046c62e6d98
Ruslan Baikulov <ruslan1123@gmail.com>
2018-07-31 01:57:51 +0300

    Bump version 0.0.3 refactor: Remove callbacks imports from __init__

--- argus/__init__.py
+++ argus/__init__.py
@@ -1,15 +1,4 @@
 from argus.model.model import Model, load_model
-from argus.engine.engine import Events
-from argus.callbacks import \
-    Callback,\
-    FunctionCallback,\
-    on_event,\
-    on_start,\
-    on_complete,\
-    on_epoch_start,\
-    on_epoch_complete,\
-    on_iteration_start,\
-    on_iteration_complete
 
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
