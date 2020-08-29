fdf44452a73d8f92b56b55dd811d476f08b281c5
ruslan <ruslan1123@gmail.com>
2020-08-29 11:26:45 +0300

    test: Use nets and models fixtures test: Start write tests for model methods

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,9 @@
 import pytest
 
+from torch import nn
+import torch.nn.functional as F
+
+from argus import Model
 from argus.loss import pytorch_losses
 from argus.optimizer import pytorch_optimizers
 
@@ -12,3 +16,84 @@ def loss_class(request):
 @pytest.fixture(scope='session', params=pytorch_optimizers.values())
 def optimizer_class(request):
     return request.param
+
+
+class LinearNet(nn.Module):
+    def __init__(self, in_features=1, out_features=1):
+        super().__init__()
+        self.fc = nn.Linear(in_features, out_features)
+
+    def forward(self, x):
+        x = self.fc(x)
+        return x
+
+
+class VisionNet(nn.Module):
+    def __init__(self, n_channels, n_classes, p_dropout=0.5):
+        super().__init__()
+        self.conv = nn.Conv2d(n_channels, 8, kernel_size=3)
+        self.p_dropout = float(p_dropout)
+        self.act = nn.ReLU()
+        self.gap = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(8, n_classes)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.act(x)
+        x = self.gap(x)
+        x = x.flatten(1)
+        if self.p_dropout:
+            x = F.dropout(x, p=self.p_dropout, training=self.training)
+        x = self.fc(x)
+        return x
+
+
+class ArgusTestModel(Model):
+    nn_module = {
+        'LinearNet': LinearNet,
+        'VisionNet': VisionNet
+    }
+
+
+@pytest.fixture(scope='session')
+def linear_net_class():
+    return LinearNet
+
+
+@pytest.fixture(scope='session')
+def vision_net_class():
+    return VisionNet
+
+
+@pytest.fixture(scope='session')
+def argus_model_class():
+    return ArgusTestModel
+
+
+@pytest.fixture(scope='function')
+def linear_argus_model_instance(argus_model_class):
+    params = {
+        'nn_module': ('LinearNet', {
+            'in_features': 16,
+            'out_features': 1
+        }),
+        'optimizer': ('Adam', {'lr': 0.001}),
+        'loss': 'MSELoss',
+        'device': 'cpu'
+    }
+    return argus_model_class(params)
+
+
+@pytest.fixture(scope='function')
+def vision_argus_model_instance(argus_model_class):
+    params = {
+        'nn_module': ('VisionNet', {
+            'n_channels': 3,
+            'n_classes': 1,
+            'p_dropout': 0.2
+        }),
+        'optimizer': ('Adam', {'lr': 0.001}),
+        'loss': 'CrossEntropyLoss',
+        'device': 'cpu'
+    }
+    return argus_model_class(params)
--- tests/model/test_build.py
+++ tests/model/test_build.py
@@ -2,7 +2,6 @@ import pytest
 
 import torch
 from torch import nn
-import torch.nn.functional as F
 
 from argus import Model
 from argus.utils import Identity
@@ -15,40 +14,10 @@ from argus.model.build import (
 )
 
 
-class LinearNet(nn.Module):
-    def __init__(self, in_features=1, out_features=1):
-        super().__init__()
-        self.fc = nn.Linear(in_features, out_features)
-
-    def forward(self, x):
-        x = self.fc(x)
-        return x
-
-
-class SimpleVisionNet(nn.Module):
-    def __init__(self, n_channels, n_classes, p_dropout=0.5):
-        super().__init__()
-        self.conv = nn.Conv2d(n_channels, 8, kernel_size=3)
-        self.p_dropout = float(p_dropout)
-        self.act = nn.ReLU()
-        self.gap = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(8, n_classes)
-
-    def forward(self, x):
-        x = self.conv(x)
-        x = self.act(x)
-        x = self.gap(x)
-        x = x.flatten(1)
-        if self.p_dropout:
-            x = F.dropout(x, p=self.p_dropout, training=self.training)
-        x = self.fc(x)
-        return x
-
-
 class TestBuild:
-    def test_simple_build(self):
-        class TestModel1(Model):
-            nn_module = LinearNet
+    def test_simple_build(self, linear_net_class):
+        class BuildModel1(Model):
+            nn_module = linear_net_class
             optimizer = torch.optim.SGD
             loss = torch.nn.MSELoss
             device = torch.device('cpu')
@@ -58,8 +27,8 @@ class TestBuild:
             'optimizer': {'lr': 0.01}
         }
 
-        model = TestModel1(params)
-        assert isinstance(model.nn_module, LinearNet)
+        model = BuildModel1(params)
+        assert isinstance(model.nn_module, linear_net_class)
         assert model.nn_module.fc.in_features == 10
         assert model.nn_module.fc.out_features == 2
         assert model.device == torch.device('cpu')
@@ -68,14 +37,14 @@ class TestBuild:
         assert isinstance(model.loss, torch.nn.MSELoss)
         assert isinstance(model.prediction_transform, Identity)
 
-    def test_default_and_string_build(self):
-        class TestModel2(Model):
-            nn_module = LinearNet
+    def test_default_and_string_build(self, linear_net_class):
+        class BuildModel2(Model):
+            nn_module = linear_net_class
             optimizer = 'Adam'
             loss = 'NLLLoss'
 
-        model = TestModel2(dict())
-        assert isinstance(model.nn_module, LinearNet)
+        model = BuildModel2(dict())
+        assert isinstance(model.nn_module, linear_net_class)
         assert model.nn_module.fc.in_features == 1
         assert model.nn_module.fc.out_features == 1
         assert model.device == torch.device('cpu')
@@ -83,9 +52,9 @@ class TestBuild:
         assert model.get_lr() == 0.001
         assert isinstance(model.loss, torch.nn.NLLLoss)
 
-    def test_default_from_params_build(self):
+    def test_default_from_params_build(self, linear_net_class):
         class TestModel3(Model):
-            nn_module = LinearNet
+            nn_module = linear_net_class
 
         params = {
             'nn_module': {'in_features': 16, 'out_features': 4},
@@ -95,7 +64,7 @@ class TestBuild:
         }
 
         model = TestModel3(params)
-        assert isinstance(model.nn_module, LinearNet)
+        assert isinstance(model.nn_module, linear_net_class)
         assert model.nn_module.fc.in_features == 16
         assert model.nn_module.fc.out_features == 4
         assert model.device == torch.device('cpu')
@@ -103,11 +72,11 @@ class TestBuild:
         assert model.get_lr() == 0.1
         assert isinstance(model.loss, torch.nn.BCEWithLogitsLoss)
 
-    def test_dict_flexibility_build(self):
-        class TestModel4(Model):
+    def test_dict_flexibility_build(self, linear_net_class, vision_net_class):
+        class BuildModel4(Model):
             nn_module = {
-                'linear': LinearNet,
-                'vision': SimpleVisionNet
+                'linear': linear_net_class,
+                'vision': vision_net_class
             }
             optimizer = {
                 'sgd': 'SGD',
@@ -117,6 +86,7 @@ class TestBuild:
                 'CrossEntropyLoss': nn.CrossEntropyLoss,
                 'nll': 'NLLLoss'
             }
+            prediction_transform = nn.Sigmoid
 
         params = {
             'nn_module': ('vision', {
@@ -129,24 +99,25 @@ class TestBuild:
             'device': 'cpu'
         }
 
-        model = TestModel4(params)
-        assert isinstance(model.nn_module, SimpleVisionNet)
+        model = BuildModel4(params)
+        assert isinstance(model.nn_module, vision_net_class)
         assert model.nn_module.conv.in_channels == 3
         assert model.nn_module.fc.out_features == 1
         assert model.device == torch.device('cpu')
         assert isinstance(model.optimizer, torch.optim.Adam)
         assert model.get_lr() == 0.01
         assert isinstance(model.loss, torch.nn.CrossEntropyLoss)
+        assert isinstance(model.prediction_transform, nn.Sigmoid)
 
-    def test_factory_function_build(self):
+    def test_factory_function_build(self, linear_net_class, vision_net_class):
         def nn_module_factory(module_name, **kwargs):
             if module_name == 'linear':
-                return LinearNet(**kwargs)
+                return linear_net_class(**kwargs)
             elif module_name == 'vision':
-                return SimpleVisionNet(**kwargs)
+                return vision_net_class(**kwargs)
             return None
 
-        class TestModel5(Model):
+        class BuildModel5(Model):
             nn_module = nn_module_factory
             optimizer = torch.optim.Adam
             loss = torch.nn.MSELoss
@@ -159,16 +130,23 @@ class TestBuild:
             }
         }
 
-        model = TestModel5(params)
-        assert isinstance(model.nn_module, SimpleVisionNet)
+        model = BuildModel5(params)
+        assert isinstance(model.nn_module, vision_net_class)
 
-        model = TestModel5({'nn_module': {'module_name': 'linear'}})
-        assert isinstance(model.nn_module, LinearNet)
+        model = BuildModel5({'nn_module': {'module_name': 'linear'}})
+        assert isinstance(model.nn_module, linear_net_class)
+
+    def test_none_nn_module_build(self):
+        class BuildModel6(Model):
+            nn_module = None
+
+        with pytest.raises(ValueError):
+            BuildModel6(dict())
 
 
 class TestCastFunction:
-    def test_cast_nn_module(self):
-        assert cast_nn_module(LinearNet) is LinearNet
+    def test_cast_nn_module(self, linear_net_class):
+        assert cast_nn_module(linear_net_class) is linear_net_class
         with pytest.raises(TypeError):
             cast_nn_module('qwerty')
 
@@ -205,3 +183,15 @@ class TestCastFunction:
         assert cast_device(['cuda:0']) == torch.device('cuda:0')
         with pytest.raises(ValueError):
             cast_device([])
+
+
+class TestBuildModelMethod:
+    def test_get_nn_module(self, linear_argus_model_instance):
+        model = linear_argus_model_instance
+        nn_module = model.nn_module
+        assert isinstance(model.nn_module, nn.Module)
+        model.nn_module = nn.parallel.DataParallel(model.nn_module)
+        assert isinstance(model.get_nn_module(), nn.Module)
+        assert isinstance(model.nn_module, nn.parallel.DataParallel)
+        assert not isinstance(model.get_nn_module(), nn.parallel.DataParallel)
+        assert model.get_nn_module() is nn_module
